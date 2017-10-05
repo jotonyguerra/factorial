0b54fc0ee7d4a317e1b15360ef683439cb83d193
Jotony Guerra <jotonyguerra@gmail.com>
2017-10-05 14:53:44 -0700

    factorial method, benchmark

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,1 +1,16 @@
-# YOUR CODE HERE
+require "benchmark"
+
+def factorial(number)
+  if number == 1 || number == 0
+    1
+  else
+   factorial(number - 1) * number
+  end
+end
+
+(1..40).each do |x|
+    puts x
+    Benchmark.bm (10) do |y|
+      y.report ("recursive:") {factorial(x)}
+    end
+end
